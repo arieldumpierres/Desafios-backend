1afb0adfa8fd34f266ce71f0815057fec22daaa8
arieldumpierres <arieldumpierres@hotmail.com>
2022-06-20 22:11:28 -0300

    se agrega desafio7

--- desafio7/mariaDB.js
+++ desafio7/mariaDB.js
@@ -2,7 +2,7 @@ export const mariaOptions = {
     client: "mysql",
     connection: {
         host: "127.0.0.1",
-        port: 3307,
+        port: 3306,
         user: "root",
         password: "",
         database: "florvioleta"
