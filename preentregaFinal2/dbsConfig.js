a9be1062f16895b4073a409b6b50cf7c6a8d7c71
arieldumpierres <arieldumpierres@hotmail.com>
2022-07-05 20:02:01 -0300

    se incluye firebase

--- preentregaFinal2/dbsConfig.js
+++ preentregaFinal2/dbsConfig.js
@@ -1,4 +1,25 @@
+
 import mongoose from "mongoose";
+import admin from "firebase-admin";
+import { readFile } from "fs/promises";
+export const FieldValue = admin.firestore.FieldValue;
+
+const serviceAccount = JSON.parse(
+    await readFile(
+        new URL("./firebaseKEY.json", import.meta.url)
+    )
+) 
+
+//FIREBASE CONFIG
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+
+export const dbFb = admin.firestore();
+export const queryProds = dbFb.collection("products");
+export const queryCart = dbFb.collection("cart");
+
 
 //MONGODB CONFIG
 export const db = mongoose.connect("mongodb+srv://ariel:lospipis2903@cluster0.0luud.mongodb.net/?retryWrites=true&w=majority", 
