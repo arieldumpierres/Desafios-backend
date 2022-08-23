ded8f3e2d96e17d2d0ff09ccd7c8fc45b7088503
arieldumpierres <arieldumpierres@hotmail.com>
2022-08-23 18:52:40 -0300

    randomrouter

--- desafio13/routers/randomRouter.js
+++ desafio13/routers/randomRouter.js
@@ -11,7 +11,7 @@ export default randomRouter;
 randomRouter.get('/', (req, res) => {
     //return res.json({puerto: req.port}) activar para verificar los puertos en los que cae esta ruta con nginx (del 8081 al 8085)
     const numeros = req.query.cant || 100000000;
-    const forked = fork('../Entregable/randomNumbers.js', ['--c', numeros]);
+    const forked = fork('../desafio13/randomNumbers.js', ['--c', numeros]);
 
     forked.on('message', nums => {
         //forked.send(numeros)
